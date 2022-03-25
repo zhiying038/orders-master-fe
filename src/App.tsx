b7e1c0cd86c1f1349910b6fc7a61e8db63db7c74
Yoh Zhi Ying <zhiying038@gmail.com>
2022-03-25 10:26:50 +0800

    setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           <Routes>
             <Route path="/" element={<OrderForm />} />
           </Routes>
-        </BrowserRouter>>
+        </BrowserRouter>
       </Layout>
     </ApolloProvider>
   );
