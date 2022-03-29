a761eae7f4fd56cdedf990350e84d832c15b8129
Yoh Zhi Ying <zhiying038@gmail.com>
2022-03-29 12:07:05 +0800

    add datepicker

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+import "flowbite";
 
 ReactDOM.render(
   <React.StrictMode>
