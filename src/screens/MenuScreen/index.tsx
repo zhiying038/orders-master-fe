d0aa9eaca8c0fe89198f1f2f684aeea026380c3c
Yoh Zhi Ying <zhiying038@gmail.com>
2022-03-29 15:15:46 +0800

    show border for item selected

--- src/screens/MenuScreen/index.tsx
+++ src/screens/MenuScreen/index.tsx
@@ -1,25 +1,44 @@
+import find from "lodash/find";
 import map from "lodash/map";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import DefaultItem from "../../assets/default-item.svg";
 import Button from "../../components/Button";
 import Modal from "../../components/Modal";
 import OrderItem from "../../components/OrderItem";
 import { useGetItemsQuery } from "../../graphql";
+import { useCart } from "../../hooks/useCart";
 
 const MenuScreen = () => {
+  const navigate = useNavigate();
+
+  const { cartItems } = useCart();
   const { data } = useGetItemsQuery();
   const [showItem, setShowItem] = useState<boolean>(false);
   const [itemSelected, setItemSelected] = useState<any>();
 
+  const foundItemInCart = (code: string) => {
+    const item = find(cartItems, (e) => e.code === code);
+    return item;
+  };
+
   return (
-    <div>
+    <div className="mt-4">
       {map(data?.getItems, (item, index) => {
+        const foundItem = foundItemInCart(item?.code);
+        const foundClass = "border-2 border-primary rounded-lg";
+
         return (
           <div
-            className="flex flex-row items-center"
+            className={`flex flex-row items-center p-2 ${
+              foundItem && foundClass
+            }`}
             key={index}
             onClick={() => {
-              setItemSelected(item);
+              setItemSelected({
+                ...item,
+                quantity: foundItem ? foundItem?.quantity : 0,
+              });
               setShowItem(true);
             }}
           >
@@ -39,12 +58,21 @@ const MenuScreen = () => {
                 </p>
               </div>
             </div>
+
+            {foundItem && (
+              <p className="text-xl font-bold mr-2">x{foundItem?.quantity}</p>
+            )}
           </div>
         );
       })}
 
-      <Button block size="large" className="mt-2" onClick={() => {}}>
-        Submit
+      <Button
+        block
+        size="large"
+        className="mt-2"
+        onClick={() => navigate("/cart")}
+      >
+        Done
       </Button>
 
       <Modal isOpen={showItem}>
